39ec9a131d9aae19d587afcb0d3a83aa7e0b080d
Tommy Smith <tommy@tommyesmith.com>
2022-04-28 14:19:05 -0400

    Add standard db CRUDMixin class

--- app/database.py
+++ app/database.py
@@ -7,7 +7,37 @@ Boolean = db.Boolean
 Integer = db.Integer
 
 
-class Model(db.Model):
+class CRUDMixin(object):
+    """
+    Adds methods for CRUD (create, read, update, delete)
+    """
+    @classmethod
+    def create(cls, **kwargs):
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+            print(attr, value)
+        if commit:
+            return self.save()
+        return self
+
+    def save(self, commit=True):
+        db.session.add(self)
+        if commit:
+            db.session.commit()
+        return self
+
+    def delete(self, commit=True):
+        db.session.delete(self)
+        if commit:
+            return db.session.commit()
+        return
+
+
+class Model(CRUDMixin, db.Model):
     __abstract__ = True
     @classmethod
     def get_all(cls, **kwargs):
